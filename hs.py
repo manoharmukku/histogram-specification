ddf41b1afe9f0d639aae535b67b5787de3da4281
Manohar Mukku <manoharmukku@gmail.com>
2018-11-06 19:25:13 +0530

    Initial get arguments

--- hs.py
+++ hs.py
@@ -1,7 +1,44 @@
 '''
 Author: Manohar Mukku
-Date: 04.11.2018
+Date: 06.11.2018
 Desc: Histogram Specification
 GitHub: https://github.com/manoharmukku/histogram-specification
 '''
 
+import sys
+import getopt
+
+def get_arguments(argv):
+    # Get the command line arguments
+    try:
+        opts, args = getopt.getopt(argv, "hi:t:", ["help", "image=", "target="])
+    except getopt.GetoptError:
+        help()
+        sys.exit(2)
+
+    image = None
+    target = None
+
+    # Parse the command line arguments
+    for opt, arg in opts:
+        if (opt in ["-h", "--help"]):
+            help()
+            sys.exit()
+        elif (opt in ["-i", "--image"]):
+            image = arg
+        elif (opt in ["-t", "--target"]):
+            target = arg
+        else:
+            print ("Invalid syntax")
+            help()
+            sys.exit()
+
+def main(argv):
+    # Get and parse the command line arguments
+    image, target = get_arguments(argv)
+
+    
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
