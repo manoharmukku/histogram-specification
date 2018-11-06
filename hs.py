7e86eb4e048eb86c7f2ea68f3026997bc943bdb7
Manohar Mukku <manoharmukku@gmail.com>
2018-11-06 21:54:26 +0530

    Read image, calculate histogram and cumulative

--- hs.py
+++ hs.py
@@ -7,6 +7,8 @@ GitHub: https://github.com/manoharmukku/histogram-specification
 
 import sys
 import getopt
+import numpy as np
+import cv2
 
 def get_arguments(argv):
     # Get the command line arguments
@@ -33,12 +35,28 @@ def get_arguments(argv):
             help()
             sys.exit()
 
+    return image, target
+
 def main(argv):
     # Get and parse the command line arguments
-    image, target = get_arguments(argv)
+    image_loc, target_hist = get_arguments(argv)
 
-    
+    # Read the image
+    img = cv2.imread(image_loc, 0)
 
+    # Calculate the input images' histogram
+    input_hist = cv2.calcHist([img], [0], None, [256], [0,256])
+
+    # Normalize the input histogram
+    total = sum(input_hist)
+    input_hist /= total
+
+    # Calculate the cumulative input histogram
+    cum_input_hist = []
+    cum = 0.0
+    for i in range(len(input_hist)):
+        cum += input_hist[i][0]
+        cum_input_hist.append(cum)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
